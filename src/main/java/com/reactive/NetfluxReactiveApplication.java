243ec8fb96ed46aca5d6152c8a4428b726cf8432
theanuragharsh <nrgharsh@gmail.com>
2023-03-28 16:49:42 +0530

    Pushing the repo || Initial commit

---
+++ src/main/java/com/reactive/NetfluxReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfluxReactiveApplication.class, args);
+	}
+
+}
